5f3dc4911afa4d7e4e905361e2c010729d43bf25
Dre-mylove <Deonlove916@gmail.com>
2023-05-12 15:34:15 -0700

    this should really be called a timeline feed

--- src/components/SharedWall/WallPost.vue
+++ src/components/SharedWall/WallPost.vue
@@ -1,75 +1,91 @@
 <template>
-
+ 
+    <v-row>
+      <v-col>
+        <ul class="image-list">
+          <li
+            v-for="(image, index) in images"
+            :key="index"
+          >
+            <v-card class="outtercard">
+              <v-card class="innercard">
+                <v-img :src="image.url" :alt="image.alt" aspect-ratio="1" cover>
+                  <template v-slot:placeholder>
+                    <v-row align="center" justify="center">
+                      <v-progress-circular
+                        indeterminate
+                        color="grey-lighten-5"
+                      ></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+              <h3>{{ image.description }}</h3>
+            </v-card>
+          </li>
+        </ul>
+      </v-col>
+    </v-row>
 </template>
 
 <script>
-
 export default {
   name: 'WallPost',
   components: {
   },
   data() {
     return {
-    }
-  }
+      images: [
+        // Add your image data here, e.g.:
+        { url: 'https://www.nzherald.co.nz/resizer/nMnOW002P5OId4ALq6KPS1P-H6M=/576x400/smart/filters:quality(70)/cloudfront-ap-southeast-2.images.arcpublishing.com/nzme/VGUXHQH4NUBOEAO3QB35PECED4.jpg', alt: 'Image 1', description: 'Crazy Night' },
+        { url: 'https://bilder.bild.de/fotos-skaliert/diddy-und-sein-smartphone--frueher-waren-sie-unzertrennlich-200732755-55154094/1,w=1192,q=low,c=0.bild.jpg', alt: 'Image 2', description: 'Fur like a Bear' },
+        { url: 'https://d.newsweek.com/en/full/1809454/puff-daddy-jennifer-lopez.jpg', alt: 'Image 3', description: 'Me and J. Lo'}
+      ],
+    };
+  },
 }
 </script>
 
 <style scoped>
-
-
-
-
-
-li{
-  text-align:center;
-  display:inline-block;
-  list-style:none;
-  position:relative;
-  border:solid 12px #fff;
-  background:#fff;
-  box-shadow:0 0 15px 0px #555;
-  transition:all 1s ease;
-  -o-transition:all 1s ease;
-  -moz-transition:all 1s ease;
-  -webkit-transition:all 1s ease;
-  top:50px;
+.outtercard {
+  background-color: rgb(255, 255, 255);
+  width: 20%; 
+  margin: auto;
+  border: 3px solid black;
+  height: 100%;
 }
 
-
-
-
-li:nth-child(1){
-    transform:rotate(10deg);
+.innercard {
+  margin: 5%;
+  margin-bottom: 10%;
+  background-color: black;
+  height: 75%;
 }
 
-li:nth-child(2){
-    transform:rotate(0deg);
+.v-img {
+  border: 2px solid black;
+  max-width: 100%; 
 }
 
-li:nth-child(3){
-    transform:rotate(-10deg);
+h3 {
+  font-family: "Shadows Into Light Two", cursive;
+  font-size: 30px;
+  line-height: 10px;
+  text-align: center;
+  margin-bottom: 10%;
 }
 
-li:nth-child(4){
-    transform:rotate(20deg);
+.image-list {
+  list-style-type: none;
+  padding: 0;
 }
 
-p{
-  margin:-15px 0 0 0;
+.image-list li {
+  text-align: center;
+  margin-bottom: 30px;
 }
 
-.light{
-  border-radius:50%;
-  position:absolute;
-  left:0;
-  right:0;
-  width:700px;
-  height:700px;
-  background:#fff;
-  filter:blur(100px);
-  opacity:0.3;
-  pointer-events:none;
+.v-col {
+  margin-top: 3vh;
 }
-
 </style>
